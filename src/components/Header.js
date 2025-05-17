45409d56916b9b0eb112f06beb3c36acb8930c06
rohitxess <xessro@gmail.com>
2025-05-17 21:20:05 +0530

    cleaned styling code

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,15 +7,15 @@ export default function Header() {
         <header className={styles.header}>
            <h1 className={styles.headerText}>Rohit Xess</h1>
            <div className={styles.navItems}>
-            <a href='#about-me' className={styles.anchor}>
-            <i className={`fa-solid fa-user-astronaut ${styles.icons}`}></i>
-            </a>
-            <a href='#experience' className={styles.anchor}>
-            <i className={`fa-solid fa-medal ${styles.icons} `}></i>
-            </a>
-            <a href='#projects' className={styles.anchor}>
-            <i className={`fa-solid fa-diagram-project ${styles.icons}`}></i>
-            </a>
+                <a href='#about-me' className={styles.anchor}>
+                    <i className={`fa-solid fa-user-astronaut ${styles.icons}`}></i>
+                </a>
+                <a href='#experience' className={styles.anchor}>
+                    <i className={`fa-solid fa-medal ${styles.icons} `}></i>
+                </a>
+                <a href='#projects' className={styles.anchor}>
+                    <i className={`fa-solid fa-diagram-project ${styles.icons}`}></i>
+                </a>
            </div>
         </header>
     )
